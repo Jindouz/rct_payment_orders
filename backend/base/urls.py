8be67fee94f7d33db86578ba95d5ac8baff49474
Jindouz <141655429+Jindouz@users.noreply.github.com>
2024-03-09 23:11:08 -0800

    Add files via upload

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('api/password-reset/', views.password_reset_request, name='password_reset_request'),
     path('api/password-reset/confirm/<int:user_id>/<str:token>/', views.password_reset_confirm, name='password_reset_confirm'),
 
-    path('products',views.products_public),
+    path('products/',views.products_public),
     path('products/<int:id>',views.products_public),
     path('authproducts', views.products),
     path('authproducts/<int:id>', views.products),
